cd59b655cfb808b9f9b283d71c961e4a0c9dc763
matheuzmelo <matheusprestesdmelo744@gmail.com>
2025-02-11 23:33:31 -0300

    feat: ajuste nas rotas do menu

--- src/app.tsx
+++ src/app.tsx
@@ -1,47 +1,40 @@
-import { CssBaseline, ThemeProvider } from "@mui/material"
-import { Suspense } from "react"
-import { HashRouter, Route, Routes } from "react-router-dom"
-import BaseLayout from "./components/layout/BaseLayout"
-import Loading from "./components/pages/Loading"
-import Login from "./components/pages/Login"
-import { menuLayout } from "./routes/menu"
-import { theme } from "./theme/theme"
-import { MenuLayout } from "./types/TMenu.type"
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Suspense } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import BaseLayout from "./components/layout/BaseLayout";
+import Loading from "./components/pages/Loading";
+import Login from "./components/pages/Login";
+import { menuLayout } from "./routes/menu";
+import { theme } from "./theme/theme";
+import { MenuLayout } from "./types/TMenu.type";
 
-export default () => {
-    const createRoutes = (menu: MenuLayout) => {
-        return menu.map(item => (
-            <Route
-                path={item.route}
-                element={
-                    <Suspense fallback={<Loading />}>
-                        {item.page}
-                    </Suspense>
-                }
-                key={item.route}
-            />
-        ))
-    }
+const App = () => {
+  const createRoutes = (menu: MenuLayout) => {
+    return menu.map((item) => (
+      <Route
+        path={item.route}
+        element={<Suspense fallback={<Loading />}>{item.page}</Suspense>}
+        key={item.route}
+      />
+    ));
+  };
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <HashRouter>
-                <Routes>
-                    {/* Rota de login separada do BaseLayout */}
-                    <Route path="/login" element={<Login />} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          {/* Rota de login separada do BaseLayout */}
+          <Route path="/login" element={<Login />} />
 
-                    {/* Rotas que utilizam o BaseLayout */}
-                    <Route
-                        path="/*"
-                        element={
-                            <BaseLayout>
-                                <Routes>{createRoutes(menuLayout)}</Routes>
-                            </BaseLayout>
-                        }
-                    />
-                </Routes>
-            </HashRouter>
-        </ThemeProvider>
-    )
-}
+          {/* Rotas que utilizam o BaseLayout */}
+        </Routes>
+        <BaseLayout>
+          <Routes>{createRoutes(menuLayout)}</Routes>
+        </BaseLayout>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+};
+
+export default App;
