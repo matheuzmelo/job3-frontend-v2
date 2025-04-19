c1e685df9be89b381b775d5e4d6b5b240ec9935a
Matheus Melo <matheusprestesdmelo744@gmail.com>
2025-04-19 18:35:04 -0300

    fix: menu

--- src/components/molecules/MenuList.tsx
+++ src/components/molecules/MenuList.tsx
@@ -3,19 +3,23 @@ import { menuLayout } from "../../routes/menu";
 import { MenuItem } from "../atoms/MenuItem";
 
 export const MenuList = ({ permissions }: { permissions: string[] }) => {
-    return (
-        <List sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "240px",
-            flexGrow: 1
-        }}>
-            {menuLayout.map(item => (
-                <MenuItem
-                    key={item.route}
-                    {...item}
-                    allowedPermissions={permissions} // Passa as permissões do usuário
-                />
-            ))}
-        </List>)
-}
+  return (
+    <List
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "240px",
+        flexGrow: 1,
+      }}
+    >
+      {[
+        ...menuLayout.filter((item) => item.name === "Home"),
+        ...menuLayout
+          .filter((item) => item.name !== "Home")
+          .sort((a, b) => a.name.localeCompare(b.name)),
+      ].map((item) => (
+        <MenuItem key={item.route} {...item} allowedPermissions={permissions} />
+      ))}
+    </List>
+  );
+};
--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -1,7 +1,17 @@
 import { LocalAtmRounded } from "@mui/icons-material";
+import BusinessIcon from "@mui/icons-material/Business";
+import ConfirmationNumberIcon from "@mui/icons-material/ConfirmationNumber";
+import GroupIcon from "@mui/icons-material/Group";
 import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import InventoryIcon from "@mui/icons-material/Inventory";
+import PersonIcon from "@mui/icons-material/Person";
 import { Documentos } from "../components/pages/Documentos";
+import { Empresas } from "../components/pages/Empresas";
 import Home from "../components/pages/Home";
+import { NotasFiscais } from "../components/pages/nfe";
+import Pessoas from "../components/pages/Pessoas";
+import Produtos from "../components/pages/Produtos";
+import Usuario from "../components/pages/Usuarios";
 import { MenuLayout } from "../types/TMenu.type";
 
 export const menuLayout: MenuLayout = [
@@ -10,14 +20,6 @@ export const menuLayout: MenuLayout = [
     route: "/",
     page: <Home />,
     icon: <HomeRoundedIcon />,
-    permission: undefined,
-  },
-  // crate a route to documentos
-  {
-    name: "Documentos",
-    route: "/documentos",
-    page: <Documentos />,
-    icon: <LocalAtmRounded />,
     permission: undefined,
   },
   // {
@@ -34,39 +36,46 @@ export const menuLayout: MenuLayout = [
   //     icon: <GroupRoundedIcon />,
   //     permission: 'CLIENTES',
   // },
-  // {
-  //     name: "Produtos",
-  //     route: "/produtos",
-  //     page: <Produtos />,
-  //     icon: <InventoryIcon />,
-  //     permission: undefined,
-  // },
-  // {
-  //     name: "Notas Fiscais",
-  //     route: "/notas-fiscais",
-  //     page: <NotasFiscais />,
-  //     icon: <LocalAtmRounded />,
-  //     permission: undefined,
-  // },
-  // {
-  //     name: "Pessoas",
-  //     route: "/pessoas",
-  //     page: <Pessoas />,
-  //     icon: <GroupIcon />,
-  //     permission: undefined
-  // },
-  // {
-  //     name: "Usuários",
-  //     route: "/usuarios",
-  //     page: <Usuario />,
-  //     icon: <PersonIcon />,
-  //     permission: undefined
-  // },
-  // {
-  //     name: "Empresas",
-  //     route: "/empresas",
-  //     page: <Empresas />,
-  //     icon: <BusinessIcon />,
-  //     permission: undefined
-  // }
+  {
+    name: "Documentos",
+    route: "/documentos",
+    page: <Documentos />,
+    icon: <ConfirmationNumberIcon />,
+    permission: "DOCUMENTOS",
+  },
+  {
+    name: "Produtos",
+    route: "/produtos",
+    page: <Produtos />,
+    icon: <InventoryIcon />,
+    permission: undefined,
+  },
+  {
+    name: "Notas Fiscais",
+    route: "/notas-fiscais",
+    page: <NotasFiscais />,
+    icon: <LocalAtmRounded />,
+    permission: undefined,
+  },
+  {
+    name: "Pessoas",
+    route: "/pessoas",
+    page: <Pessoas />,
+    icon: <GroupIcon />,
+    permission: undefined,
+  },
+  {
+    name: "Usuários",
+    route: "/usuarios",
+    page: <Usuario />,
+    icon: <PersonIcon />,
+    permission: undefined,
+  },
+  {
+    name: "Empresas",
+    route: "/empresas",
+    page: <Empresas />,
+    icon: <BusinessIcon />,
+    permission: undefined,
+  },
 ];
